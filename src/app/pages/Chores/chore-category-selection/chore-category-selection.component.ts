71f9c41a433db4e8b4d585ed5401d330eab587e0
DevAmaruk <j.guthauser@ik.me>
2025-04-16 10:42:42 +0200

    refactor: Added general comments at the top of the script

--- src/app/pages/Chores/chore-category-selection/chore-category-selection.component.ts
+++ src/app/pages/Chores/chore-category-selection/chore-category-selection.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-chore-category-selection',
-  imports: [],
-  templateUrl: './chore-category-selection.component.html',
-  styleUrl: './chore-category-selection.component.scss'
-})
-export class ChoreCategorySelectionComponent {
+/* 
+This component is responsible for selecting a chore category. 
+It will be displayed as small cards with icons and texts that the user can click on.
+Each card will open a new page with the tasks of that category.
+At the bottom there will be a menu with the following 
+*/
 
-}
+@Component({
+	selector: 'app-chore-category-selection',
+	imports: [],
+	templateUrl: './chore-category-selection.component.html',
+	styleUrl: './chore-category-selection.component.scss',
+})
+export class ChoreCategorySelectionComponent {}
