6de23c7b9709db85ab54472c44f68572b39bf426
DevAmaruk <j.guthauser@ik.me>
2025-04-16 09:50:05 +0200

    feat(firebase): Added Angular Fire to project

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,13 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "homeflow-cce5c", appId: "1:481811143395:web:2ef8db79276075bb2bc3d6", storageBucket: "homeflow-cce5c.firebasestorage.app", apiKey: "AIzaSyDF4H-EfCAc6MQM7LsncLvWepnqVSkBK4I", authDomain: "homeflow-cce5c.firebaseapp.com", messagingSenderId: "481811143395" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()),
   ],
 };
