7bea6fa7dd5ec95d4a3c2e1a15f79dd604005083
DevAmaruk <j.guthauser@ik.me>
2025-04-17 11:32:03 +0200

    feat: Add route for Cat and Task

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: 'category',
+		loadComponent: () =>
+			import('./pages/Chores/chore-category-selection/chore-category-selection.component').then(
+				c => c.ChoreCategorySelectionComponent,
+			),
+	},
+	{
+		path: 'tasks/:categoryId',
+		loadComponent: () =>
+			import('./pages/Chores/chore-task-selection/chore-task-selection.component').then(c => c.ChoreTaskSelectionComponent),
+	},
+	{
+		path: '',
+		redirectTo: 'category',
+		pathMatch: 'full',
+	},
+];
