b67a66592f0b9d2b09af528cb6465d9d07a24709
DevAmaruk <j.guthauser@ik.me>
2025-04-17 10:27:19 +0200

    refactor: Modified JSON structure
    Decided to use an array of categories for better use.

--- src/app/interfaces/categories.ts
+++ src/app/interfaces/categories.ts
@@ -1,4 +1,4 @@
-export interface Categories {
+export interface Category {
 	uuid: string;
 	title: string;
 	icon: string;
--- src/app/pages/Chores/chore-category-selection/chore-category-selection.component.ts
+++ src/app/pages/Chores/chore-category-selection/chore-category-selection.component.ts
@@ -4,7 +4,12 @@ import { Component } from '@angular/core';
 This component is responsible for selecting a chore category. 
 It will be displayed as small cards with icons and texts that the user can click on.
 Each card will open a new page with the tasks of that category.
-At the bottom there will be a menu with the following 
+At the bottom there will be a menu with the following :
+
+- Calendar: Open the Calendar page
+- Task List: Open the "homepage" of the chore section with the list of all tasks for the current day.
+- Scoreboard: Open the scoreboard page
+- Settings: Open the settings modal (TO THINK MORE ABOUT THIS ONE)
 */
 
 @Component({
