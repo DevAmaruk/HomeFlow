f5557ff4bbe786860c4c0da11a0bbd0fa1e32852
Jonathan Guthauser <j.guthauser@ik.me>
2025-04-17 23:33:44 +0200

    feat: connected task service to get activated tasks

--- src/app/pages/Chores/chore-list-homepage/chore-list-homepage.component.ts
+++ src/app/pages/Chores/chore-list-homepage/chore-list-homepage.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-import { Task } from '../../../interfaces/categories';
 import { TaskSelectionService } from '../../../services/tasks/task-selection.service';
+import { Task } from '../../../interfaces/categories';
+import { RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { map, Observable } from 'rxjs';
+import { collection, collectionData, deleteDoc, doc, Firestore, getDocs, query, where } from '@angular/fire/firestore';
 
 @Component({
 	selector: 'app-chore-list-homepage',
-	imports: [CommonModule],
+	imports: [RouterLink, CommonModule],
 	templateUrl: './chore-list-homepage.component.html',
 	styleUrl: './chore-list-homepage.component.scss',
 })
 export class ChoreListHomepageComponent implements OnInit {
-	async ngOnInit() {}
-	constructor(public readonly _taskSeletionService: TaskSelectionService) {}
+	public selectedTasks: Task[] = [];
+
+	public taskDescription$?: Observable<string[]>;
+
+	constructor(private readonly _taskSelectionService: TaskSelectionService) {}
+
+	async ngOnInit() {
+		this.taskDescription$ = this._taskSelectionService.taskDescription$;
+	}
 }
