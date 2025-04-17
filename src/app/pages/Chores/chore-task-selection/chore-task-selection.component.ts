fcd4f648b694e48312f5ec2dd1d5f1d6b50cf0da
DevAmaruk <j.guthauser@ik.me>
2025-04-17 11:34:33 +0200

    feat: Displays tasks for a selected category
    Implements the display of tasks associated with a specific chore category.
    Retrieves the category ID from the route parameters and uses it to fetch
    and display the tasks related to that category.

--- src/app/pages/Chores/chore-task-selection/chore-task-selection.component.ts
+++ src/app/pages/Chores/chore-task-selection/chore-task-selection.component.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ChoreCategoryService } from '../../../services/chores/chore-category.service';
+import { Category, Task } from '../../../interfaces/categories';
+
+/*
+This component is responsible for selecting a chore task based on the selected category.
+It will also be displayed on a card.
+Upon selecting a task, the chore-list array is updated with the selected task.
+So we need to push the selected task to the chore-list array, which will be displayed on the Chore-List Homepage component.
+*/
 
 @Component({
-  selector: 'app-chore-task-selection',
-  imports: [],
-  templateUrl: './chore-task-selection.component.html',
-  styleUrl: './chore-task-selection.component.scss'
+	selector: 'app-chore-task-selection',
+	imports: [],
+	templateUrl: './chore-task-selection.component.html',
+	styleUrl: './chore-task-selection.component.scss',
 })
-export class ChoreTaskSelectionComponent {
+export class ChoreTaskSelectionComponent implements OnInit {
+	// Variable to store the task from a specific category
+	public tasks?: Task[] | undefined;
 
+	/*
+  First we need to inject the ActivatedRoute to get the categoryId from the URL.
+  Then we can use this categoryId to get the tasks of that category.
+  */
+	constructor(private readonly _activeRoute: ActivatedRoute, private readonly _choreCatService: ChoreCategoryService) {}
+
+	async ngOnInit() {
+		this.getTasks();
+	}
+
+	async getTasks() {
+		const categoryId: string | null = this._activeRoute.snapshot.paramMap.get('categoryId');
+		if (categoryId) {
+			const categories: Category[] = await this._choreCatService.getChoreCategories();
+			const selectedCategory: Category | undefined = categories.find(cat => cat.uuid === categoryId);
+			this.tasks = selectedCategory!.tasks;
+		}
+	}
 }
