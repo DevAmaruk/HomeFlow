96639fc3bafda147bb8e9449fa7bd9c3e4e3fa81
Jonathan Guthauser <j.guthauser@ik.me>
2025-04-22 23:48:02 +0200

    refactor: Added ChoreHomepage component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { ChoreListHomepageComponent } from './pages/Chores/chore-list-homepage/chore-list-homepage.component';
 
 export const routes: Routes = [
 	{
