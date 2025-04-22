11f42caf4260a48a678893d8554d6b5eaa95b94e
DevAmaruk <j.guthauser@ik.me>
2025-04-22 15:21:42 +0200

    refactor: onSigning methods improved

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,9 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../../services/auth/auth.service';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { AuthErrorCodes, User, UserCredential } from '@angular/fire/auth';
+import { User } from '@angular/fire/auth';
 import { Router } from '@angular/router';
-import { FirebaseError } from '@angular/fire/app';
 import { doc, Firestore, setDoc } from '@angular/fire/firestore';
 
 @Component({
@@ -15,9 +14,9 @@ import { doc, Firestore, setDoc } from '@angular/fire/firestore';
 })
 export class LoginComponent implements OnInit {
 	public signUpForm: FormGroup = new FormGroup({});
+	public signInForm: FormGroup = new FormGroup({});
 
-	private userCredential: UserCredential | null = null;
-	public user?: User | null;
+	public user: User | undefined;
 
 	constructor(
 		private readonly _authService: AuthService,
@@ -26,92 +25,93 @@ export class LoginComponent implements OnInit {
 	) {}
 
 	async ngOnInit() {
-		await this.createSignUpForm();
+		this.createSignUpForm();
+		this.createSignInForm();
 		// this.onAnonymousSignIn();
 	}
 
-	async createSignUpForm() {
+	public createSignUpForm() {
 		this.signUpForm = new FormGroup({
 			email: new FormControl(''),
 			password: new FormControl(''),
 		});
 	}
 
-	// Anonymous sign in method
-	async onAnonymousSignIn() {
-		try {
-			this.userCredential = await this._authService.anonymousSignIn();
-			this.user = this.userCredential.user;
-			console.log('User signed in anonymously:', this.user.uid);
-		} catch (error) {
-			if (error instanceof Error) {
-				console.error('Error signing in anonymously:', error.message);
-			}
-		}
+	public createSignInForm() {
+		this.signInForm = new FormGroup({
+			email: new FormControl(''),
+			password: new FormControl(''),
+		});
 	}
 
-	async onSignUp() {
-		const { email, password } = this.signUpForm.value;
-		try {
-			this.userCredential = await this._authService.signUp(email, password);
-			this.user = this.userCredential.user;
-			const userEmail = this.user.email;
+	// // Anonymous sign in method
+	// async onAnonymousSignIn() {
+	// 	try {
+	// 		this.userCredential = await this._authService.anonymousSignIn();
+	// 		this.user = this.userCredential.user;
+	// 		console.log('User signed in anonymously:', this.user.uid);
+	// 	} catch (error) {
+	// 		if (error instanceof Error) {
+	// 			console.error('Error signing in anonymously:', error.message);
+	// 		}
+	// 	}
+	// }
 
-			const userDocRef = doc(this._firestore, 'Users', userEmail!);
+	public async onSignUp() {
+		try {
+			const { email, password } = this.signUpForm.value;
+
+			// We get the user from the AuthService
+			this.user = await this._authService.signUp(email, password);
+
+			// Check if the user is null of undefined
+			if (!this.user) {
+				throw new Error('User is null or undefined after sign up.');
+			}
+
+			// We get the email from the current user signing up
+			const userUid = this.user.uid;
+
+			// Check if the email is null of undefined
+			if (!userUid) {
+				throw new Error('User uid is null or undefined.');
+			}
+
+			// We create a ref to the document in the Users collection using the user uid.
+			const userDocRef = doc(this._firestore, 'Users', userUid);
+
+			//We set the document with the uid of the signed up user and add the fields email and uid inside.
 			await setDoc(userDocRef, {
 				email: this.user.email,
 				uid: this.user.uid,
 			});
 
+			this.signUpForm.reset();
+
 			this._route.navigate(['/family']);
 			console.log('User signed up successfully:', this.user.email);
 		} catch (error) {
-			if (error instanceof FirebaseError) {
-				switch (error.code) {
-					case AuthErrorCodes.WEAK_PASSWORD:
-						console.log('Weak password:', error.code);
-						break;
-					case AuthErrorCodes.INVALID_EMAIL:
-						console.log('Invalid email:', error.code);
-						break;
-					case AuthErrorCodes.EMAIL_EXISTS:
-						console.log('Email already exists:', error.code);
-						break;
-					default:
-						console.error('Error signing up:', error.message);
-				}
-			}
+			console.error('Error signing up:', error);
 		}
 	}
 
-	async onSignIn() {
-		const { email, password } = this.signUpForm.value;
+	public async onSignIn() {
 		try {
-			this.userCredential = await this._authService.signIn(email, password);
-			this.user = this.userCredential.user;
+			const { email, password } = this.signInForm.value;
+
+			this.user = await this._authService.signIn(email, password);
+
+			if (!this.user) {
+				throw new Error('User is null or undefined after sign in.');
+			}
+
 			console.log('User signed in successfully:', this.user.email);
+
+			this.signInForm.reset();
+
 			this._route.navigate(['/chore-homepage']);
 		} catch (error) {
-			if (error instanceof FirebaseError) {
-				if (error.code === AuthErrorCodes.INVALID_PASSWORD) {
-					console.log('Invalid password:', error.code);
-				} else if (error.code === AuthErrorCodes.USER_DELETED) {
-					console.log('User not found:', error.code);
-				} else if (error.code === AuthErrorCodes.INVALID_EMAIL) {
-					console.log('Invalid email:', error.code);
-				} else if (error.code === AuthErrorCodes.CREDENTIAL_ALREADY_IN_USE) {
-					console.log('Credential already in use:', error.code);
-				}
-			}
-		}
-	}
-
-	async onSignOut() {
-		try {
-			await this._authService.signOut();
-			console.log('User signed out successfully');
-		} catch (error) {
-			console.error('Error signing out:', error);
+			console.error('Error signing in:', error);
 		}
 	}
 }
