93807d036a070d3e761e2d8e91055dcad5aad4a4
Jonathan Guthauser <j.guthauser@ik.me>
2025-04-30 01:09:23 +0200

    feat: added the visual for the calendar page

--- src/app/pages/Chores/calendar/calendar.component.ts
+++ src/app/pages/Chores/calendar/calendar.component.ts
@@ -8,20 +8,40 @@ import { TaskSelectionService } from '../../../services/tasks/task-selection.ser
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { Tasks } from '../../../interfaces/category';
+import {
+	IonCard,
+	IonCardContent,
+	IonCol,
+	IonContent,
+	IonGrid,
+	IonRow,
+} from '@ionic/angular/standalone';
 
 /*
 This page is used to display the calendar for the chores. 
 When the user clicks on a date, it will show below the chores for that date.
 */
 
+const ionicElements = [
+	IonContent,
+	IonGrid,
+	IonRow,
+	IonCol,
+	IonCard,
+	IonCardContent,
+];
+
 @Component({
 	selector: 'app-calendar',
-	imports: [FullCalendarModule, CommonModule],
+	imports: [FullCalendarModule, CommonModule, ...ionicElements],
 	templateUrl: './calendar.component.html',
 	styleUrl: './calendar.component.scss',
 })
 export class CalendarComponent {
-	constructor(private readonly _taskSelectionService: TaskSelectionService, private readonly _router: Router) {}
+	constructor(
+		private readonly _taskSelectionService: TaskSelectionService,
+		private readonly _router: Router,
+	) {}
 
 	public tasksForSelectedDate: Tasks[] = []; // Store tasks for the selected date
 	public selectedDate: string | null = null; // Store the selected date
@@ -33,15 +53,17 @@ export class CalendarComponent {
 		contentHeight: 'auto',
 		showNonCurrentDates: false,
 		firstDay: 1,
-		headerToolbar: {
-			center: 'prevYear,nextYear',
-		},
 		locale: frLocale,
 		timeZone: 'local',
+		selectLongPressDelay: 50,
+		themeSystem: 'standard',
 		select: async selectionInfo => {
 			this.selectedDate = selectionInfo.startStr;
-			const allTasks = await this._taskSelectionService.getTasksFromFamillyGroup();
-			this.tasksForSelectedDate = allTasks.filter(task => task.dueDate === this.selectedDate);
+			const allTasks =
+				await this._taskSelectionService.getTasksFromFamillyGroup();
+			this.tasksForSelectedDate = allTasks.filter(
+				task => task.dueDate === this.selectedDate,
+			);
 		},
 	};
 
