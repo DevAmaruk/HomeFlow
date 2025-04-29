0f57e94b6c20481dd068c25818f80c7c520635f4
Jonathan Guthauser <j.guthauser@ik.me>
2025-04-30 01:08:39 +0200

    feat: added children to homepage menu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,48 +3,85 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
 	{
 		path: 'login-homepage',
-		loadComponent: () => import('./pages/login/login-homepage.component').then(c => c.LoginHomepageComponent),
+		loadComponent: () =>
+			import('./pages/login/login-homepage.component').then(
+				c => c.LoginHomepageComponent,
+			),
 	},
 	{
 		path: 'sign-up',
-		loadComponent: () => import('./pages/login/login.component').then(c => c.LoginComponent),
+		loadComponent: () =>
+			import('./pages/login/login.component').then(c => c.LoginComponent),
 	},
 	{
 		path: 'sign-in',
-		loadComponent: () => import('./pages/sign-in-page/sign-in-page.component').then(c => c.SignInPageComponent),
+		loadComponent: () =>
+			import('./pages/sign-in-page/sign-in-page.component').then(
+				c => c.SignInPageComponent,
+			),
 	},
 	{
-		path: 'family',
-		loadComponent: () => import('./pages/familly/familly.component').then(c => c.FamillyComponent),
+		path: 'familly',
+		loadComponent: () =>
+			import('./pages/familly/familly.component').then(c => c.FamillyComponent),
 	},
 	{
-		path: 'chore-homepage',
-		loadComponent: () => import('./pages/Chores/chore-homepage/chore-homepage.component').then(c => c.ChoreHomepageComponent),
+		path: 'chore-menu-homepage',
+		loadComponent: () =>
+			import(
+				'./pages/Chores/chore-menu-homepage/chore-menu-homepage.component'
+			).then(c => c.ChoreMenuHomepageComponent),
+		children: [
+			{
+				path: 'chore-homepage',
+				loadComponent: () =>
+					import('./pages/Chores/chore-homepage/chore-homepage.component').then(
+						c => c.ChoreHomepageComponent,
+					),
+			},
+			{
+				path: 'calendar',
+				loadComponent: () =>
+					import('./pages/Chores/calendar/calendar.component').then(
+						c => c.CalendarComponent,
+					),
+			},
+			{
+				path: '',
+				redirectTo: '/chore',
+				pathMatch: 'full',
+			},
+		],
 	},
+
 	{
 		path: 'category',
 		loadComponent: () =>
-			import('./pages/Chores/chore-category-selection/chore-category-selection.component').then(
-				c => c.ChoreCategorySelectionComponent,
-			),
+			import(
+				'./pages/Chores/chore-category-selection/chore-category-selection.component'
+			).then(c => c.ChoreCategorySelectionComponent),
 	},
 	{
 		path: 'tasks/:categoryId',
 		loadComponent: () =>
-			import('./pages/Chores/chore-task-selection/chore-task-selection.component').then(c => c.ChoreTaskSelectionComponent),
+			import(
+				'./pages/Chores/chore-task-selection/chore-task-selection.component'
+			).then(c => c.ChoreTaskSelectionComponent),
 	},
 	{
 		path: 'chore-edition/:taskId',
-		loadComponent: () => import('./pages/Chores/chore-edition/chore-edition.component').then(c => c.ChoreEditionComponent),
+		loadComponent: () =>
+			import('./pages/Chores/chore-edition/chore-edition.component').then(
+				c => c.ChoreEditionComponent,
+			),
 	},
-	{
-		path: 'calendar',
-		loadComponent: () => import('./pages/Chores/calendar/calendar.component').then(c => c.CalendarComponent),
-	},
+
 	{
 		path: 'custom-task-creation',
 		loadComponent: () =>
-			import('./pages/Chores/custom-task-creation/custom-task-creation.component').then(c => c.CustomTaskCreationComponent),
+			import(
+				'./pages/Chores/custom-task-creation/custom-task-creation.component'
+			).then(c => c.CustomTaskCreationComponent),
 	},
 	{
 		path: '',
