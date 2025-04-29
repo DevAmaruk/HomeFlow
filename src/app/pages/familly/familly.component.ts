06b854ea2ab9c4c94846b569177326a05c14e073
Jonathan Guthauser <j.guthauser@ik.me>
2025-04-30 01:11:20 +0200

    feat: added the visuals for the familly page

--- src/app/pages/familly/familly.component.ts
+++ src/app/pages/familly/familly.component.ts
@@ -1,23 +1,52 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { FamillyService } from '../../services/famillyService/familly.service';
+import {
+	IonContent,
+	IonGrid,
+	IonInput,
+	IonButton,
+	IonCol,
+	IonRow,
+	IonText,
+} from '@ionic/angular/standalone';
 
 /*
 This component will ask the user to create a family group.
 The input value will be used to create the collection for the user.
 */
 
+const ionicElements = [
+	IonContent,
+	IonInput,
+	IonButton,
+	IonGrid,
+	IonCol,
+	IonRow,
+	IonText,
+];
+
 @Component({
 	selector: 'app-familly',
-	imports: [],
+	imports: [...ionicElements],
 	templateUrl: './familly.component.html',
 	styleUrl: './familly.component.scss',
 })
 export class FamillyComponent {
-	constructor(private readonly _router: Router, private readonly _famillyService: FamillyService) {}
+	constructor(
+		private readonly _router: Router,
+		private readonly _famillyService: FamillyService,
+	) {}
 
-	public async createFamilly(famillyInputName: string) {
-		await this._famillyService.createFamillyGroup(famillyInputName); // Call the service to create the family group
-		this._router.navigate(['/chore-homepage']);
+	public async createFamilly(famillyInputName: IonInput) {
+		try {
+			const famillyName = famillyInputName.value as string;
+			if (famillyName) {
+				await this._famillyService.createFamillyGroup(famillyName);
+				this._router.navigate(['/chore-homepage']);
+			}
+		} catch (error) {
+			console.error('Error creating family group:', error);
+		}
 	}
 }
