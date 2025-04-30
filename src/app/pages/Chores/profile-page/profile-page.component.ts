c79a63dd126ec6487cc1d53f94d42931ace3e6cf
Jonathan Guthauser <j.guthauser@ik.me>
2025-04-30 17:08:13 +0200

    feat: visuals for the profile page modal

--- src/app/pages/Chores/profile-page/profile-page.component.ts
+++ src/app/pages/Chores/profile-page/profile-page.component.ts
@@ -1,13 +1,19 @@
 import { Component, Input } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import {
+	IonAvatar,
 	IonButton,
 	IonButtons,
+	IonCol,
 	IonContent,
+	IonGrid,
 	IonHeader,
+	IonImg,
 	IonInput,
 	IonItem,
 	IonLabel,
+	IonRow,
+	IonText,
 	IonToolbar,
 	ModalController,
 } from '@ionic/angular/standalone';
@@ -19,7 +25,21 @@ import { Router } from '@angular/router';
 This page serves as profile page where the user can log out, change its username, profile picture, etc. 
 */
 
-const ionicElements = [IonContent, IonHeader, IonToolbar, IonButtons, IonButton, IonItem, IonLabel, IonInput];
+const ionicElements = [
+	IonContent,
+	IonHeader,
+	IonToolbar,
+	IonButtons,
+	IonButton,
+	IonLabel,
+	IonInput,
+	IonAvatar,
+	IonImg,
+	IonGrid,
+	IonRow,
+	IonCol,
+	IonText,
+];
 
 @Component({
 	selector: 'app-profile-page',
@@ -34,6 +54,10 @@ export class ProfilePageComponent {
 		username: new FormControl(''),
 	});
 
+	public userEmailForm: FormGroup = new FormGroup({
+		email: new FormControl('', Validators.email),
+	});
+
 	constructor(
 		private readonly _modalCtrl: ModalController,
 		private readonly _authService: AuthService,
@@ -42,10 +66,25 @@ export class ProfilePageComponent {
 	) {}
 
 	async ionViewWillEnter() {
-		console.log('User:', this.user);
 		this.userNameForm.patchValue({ username: this.user?.displayName });
-		console.log('UserNameForm:', this.userNameForm.value.username);
+		this.userEmailForm.patchValue({ email: this.user?.email });
 	}
+
+	// public async updateEmail() {
+	// 	try {
+	// 		const { email } = this.userEmailForm.value;
+
+	// 		await this._authService.updateUserEmail(email);
+
+	// 		// Update email in Firestore Users collection
+	// 		const userDocRef = doc(this._firestore, 'Users', this.user.uid);
+	// 		await updateDoc(userDocRef, { email });
+
+	// 		console.log('Email updated successfully:', email);
+	// 	} catch (error) {
+	// 		console.error('Error updating email:', error);
+	// 	}
+	// }
 
 	public async updateUsername() {
 		try {
@@ -55,7 +94,6 @@ export class ProfilePageComponent {
 			const userDocRef = doc(this._firestore, 'Users', this.user.uid);
 			await updateDoc(userDocRef, { username });
 			console.log('Username updated successfully:', username);
-			this.closeModal();
 		} catch (error) {
 			console.error('Error updating username:', error);
 		}
