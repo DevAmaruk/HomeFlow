129e158fd4d92320306e5168b8ffb0e898e062c5
Jonathan Guthauser <j.guthauser@ik.me>
2025-04-30 23:57:39 +0200

    feat: added visuals for the email verification

--- src/app/modals/email-verification-modal/email-verification-modal.component.ts
+++ src/app/modals/email-verification-modal/email-verification-modal.component.ts
@@ -1,12 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-import { IonButton, IonContent, IonHeader, IonTitle, IonToolbar, ModalController } from '@ionic/angular/standalone';
+import {
+	IonButton,
+	IonCol,
+	IonContent,
+	IonGrid,
+	IonHeader,
+	IonRow,
+	IonText,
+	IonTitle,
+	IonToolbar,
+	ModalController,
+} from '@ionic/angular/standalone';
 import { AuthService } from '../../services/auth/auth.service';
 
 @Component({
 	selector: 'app-email-verification-modal',
 	templateUrl: './email-verification-modal.component.html',
 	styleUrls: ['./email-verification-modal.component.scss'],
-	imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton],
+	imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonGrid, IonRow, IonCol, IonText],
 })
 export class EmailVerificationModalComponent {
 	constructor(private readonly _modalCtrl: ModalController, private readonly _authService: AuthService) {}
