1a4dee6f2c48a8f6d1e4e9793387b9200c905f0e
Jonathan Guthauser <j.guthauser@ik.me>
2025-04-30 17:54:58 +0200

    feat: PWA packages installed and setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import * as allIcons from 'ionicons/icons';
+import '@khmyznikov/pwa-install';
 
 @Component({
 	selector: 'app-root',
 	imports: [IonApp, IonRouterOutlet],
 	templateUrl: './app.component.html',
 	styleUrl: './app.component.scss',
+	schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppComponent {
 	constructor() {
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
@@ -7,6 +7,7 @@ import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { environment } from '../environments/environment';
 import { provideHttpClient } from '@angular/common/http';
 import { provideIonicAngular } from '@ionic/angular/standalone';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
 	providers: [
@@ -17,5 +18,9 @@ export const appConfig: ApplicationConfig = {
 		provideAuth(() => getAuth()),
 		provideFirestore(() => getFirestore()),
 		provideIonicAngular({}),
+		provideServiceWorker('ngsw-worker.js', {
+			enabled: !isDevMode(),
+			registrationStrategy: 'registerWhenStable:30000',
+		}),
 	],
 };
